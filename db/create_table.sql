3b22e7b307f54a58d9d2cd2fccb7ec66f4250400
Pavel Syrovatskii <pavel1107syr@gmail.com>
2023-10-13 19:14:05 +0300

    fix table

--- db/create_table.sql
+++ db/create_table.sql
@@ -4,13 +4,13 @@
 CREATE TABLE resources_schema.resource_types (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    max_speed DECIMAL(5, 2) NOT NULL
+    max_speed INTEGER NOT NULL
 );
 -- Таблица для  ресурсов
 CREATE TABLE resources_schema.resources (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    current_speed DECIMAL(5, 2),
+    current_speed INTEGER,
     resource_type_id INT,
-    FOREIGN KEY (resource_type_id) REFERENCES resources_schema.resource_types(id)
+    FOREIGN KEY (resource_type_id) REFERENCES resources_schema.resource_types(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
